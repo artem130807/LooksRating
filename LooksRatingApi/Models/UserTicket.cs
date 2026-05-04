dd640149a9db746235bc018809da9915203962aa
artem1308 <artemsv1308@gmail.com>
2026-05-04 22:27:35 +0400

    Add DockerFile

--- LooksRatingApi/Models/UserTicket.cs
+++ LooksRatingApi/Models/UserTicket.cs
@@ -9,20 +9,8 @@ namespace LooksRatingApi.Models
     public class UserTicket
     {
         public Guid Id { get; set; }
-
-        /// <summary>
-        /// Причина жалобы.
-        /// </summary>
         public string Description { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Когда произошло нарушение.
-        /// </summary>
         public DateTime OccuredAt { get; set; }
-
-        /// <summary>
-        /// Пользователь, на которого пожаловались.
-        /// </summary>
         public Guid UserId { get; set; }
         public User User { get; set; } = null!;
     }
